b745350791f07c9444d189344168934089725ae3
antonia.moschin <antonia.moschinn>
2019-06-12 08:20:57 +0200

    Aktivitäten

--- aktivitaeten/Ebike.js
+++ aktivitaeten/Ebike.js
@@ -1,4 +1,4 @@
-{
+const Ebike{
     "displayFieldName": "",
     "fieldAliases": {
         "FID": "FID",
--- aktivitaeten/main.js
+++ aktivitaeten/main.js
@@ -38,7 +38,7 @@ karte.setView(
 //Fullscreen
 karte.addControl(new L.Control.Fullscreen());
 
-//Mit Mausklick Koordinaten anzeigen lassen
+/* //Mit Mausklick Koordinaten anzeigen lassen
 var coords = new L.Control.Coordinates();
 coords.addTo(karte);
 karte.on('click', function (e) {
@@ -60,4 +60,44 @@ L.geoJson()
     `;
 }) //Windgeschwindigkeit mit if abfrage, wenn keine Daten vorhanden sind
 .addTo(awsTirol);
+ */
+//GPX GRuppe erstellen und Menü
+let bike = L.featureGroup().addTo(karte);
+layerControl.addOverlay(bike, "Ebike");
 
+//Höhenprofil intitalisieren
+//let controlElevation = null;
+
+//GPX Track laden
+console.log(Ebike.features.geometry);
+
+bike.clearLayers(); //nur einen Track anzeigen und der der vorher angezeigt wurde wird nicht mehr angezeigt
+const biken = new L.GPX("ebike.gpx", {
+    async: true,
+    marker_options: {
+        startIconUrl: 'icons/pin-icon-start.png',
+        endIconUrl: 'icons/pin-icon-end.png',
+        shadowUrl: 'icons/pin-shadow.png',
+        iconSize: [32, 37]
+    }
+}).addTo(bike);
+
+bike.on("loaded", function () {
+    // karte.fitBounds(gpxTrack.getBounds());
+});
+//Höhenprofil zeichnen das sich aktualisiert
+
+bike.on("addline", function (evt) {
+    //damit immer nur eine Elevation angezeigt wird/ bestehendes Profil löschen
+    if (controlElevation) {
+        controlElevation.clear();
+        document.getElementById("elevation-div").innerHTML = "";
+    }
+    controlElevation = L.control.elevation({
+        theme: "steelblue-theme",
+        detachedView: true,
+        elevationDiv: "#elevation-div"
+    })
+    controlElevation.addTo(karte);
+    controlElevation.addData(evt.line);
+})
